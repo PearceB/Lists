90a4f5e52a07fcba181dfa6fce50f183c0f66181
PearceB <Bobtarznit@gmail.com>
2013-01-22 12:45:01 -0600

    Exercise 118- Data Definition

---
+++ exercise 118.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |exercise 118|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Exercise 118
+
+
+; A List-of-numbers is one of: 
+; – empty
+; – (cons Number List-of-numbers)
