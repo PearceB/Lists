fccb7234f0fb9e33cd40a3b379e1de5f41886f9f
Pearce <Bobtarznit@gmail.com>
2013-01-23 22:23:26 -0600

    Exercise 121
    Data Defintion

---
+++ exercise 121.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |exercise 121|) (read-case-sensitive #t) (teachpacks ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp")))))
+; Exercise 121
+
+; A List-of-Images is one of:
+; - empty
+; - (cons Image List-of-Images)
+; interp. a List-of-Images is a list of images
