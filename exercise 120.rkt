9dcd16fcaca1c743a7eaeb9e0acb641a883f5e2b
Pearce <Bobtarznit@gmail.com>
2013-01-23 22:10:41 -0600

    Exercise 120
    Data Definition

---
+++ exercise 120.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |exercise 120|) (read-case-sensitive #t) (teachpacks ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp")))))
+; Exercise 120
+
+; A List-of-Strings is one of:
+; - empty
+; - (cons String List-of-Strings)
+; interp. a List-of-Strings is a list of strings
