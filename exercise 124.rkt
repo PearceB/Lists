3bd41317706afacb41450608bfab1380bc6fb050
Pearce <Bobtarznit@gmail.com>
2013-01-28 18:27:12 -0600

    Exercise 124
    Data Definition

---
+++ exercise 124.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |exercise 124|) (read-case-sensitive #t) (teachpacks ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.ss" "teachpack" "2htdp") (lib "image.ss" "teachpack" "2htdp")))))
+; Exercise 124
+
+; A NEList-of-temperatures
+; - (cons CTemperature empty)
+; - (cons CTemperature NEList-of-temperatures)
+; interp. : nopn-empty lists of measured temperatures
